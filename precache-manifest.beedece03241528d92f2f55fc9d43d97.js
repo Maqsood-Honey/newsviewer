763ae79eae01d60e5be6af688ec9f860dc904ed1
Maqsood <maqsoodmanzoor@gmail.com>
2023-07-19 18:37:30 +0500

    Updates

---
+++ precache-manifest.beedece03241528d92f2f55fc9d43d97.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "0df226344de7a1ae5f16",
+    "url": "/newsviewer/static/css/main.dbe8c483.chunk.css"
+  },
+  {
+    "revision": "0df226344de7a1ae5f16",
+    "url": "/newsviewer/static/js/main.702060ba.chunk.js"
+  },
+  {
+    "revision": "87d22f5aa02ae8f182f7",
+    "url": "/newsviewer/static/js/runtime~main.3d5e8d54.js"
+  },
+  {
+    "revision": "e2a02c4cca8c3241515e",
+    "url": "/newsviewer/static/js/2.1f8f3dcb.chunk.js"
+  },
+  {
+    "revision": "03c2247ebef71b42a125",
+    "url": "/newsviewer/static/js/3.133d7e0c.chunk.js"
+  },
+  {
+    "revision": "c2b0a9b5de35d47ee87e19647a50de67",
+    "url": "/newsviewer/static/media/Spinner.c2b0a9b5.gif"
+  },
+  {
+    "revision": "7a2dfe4e691914132be4a56e14c54a5a",
+    "url": "/newsviewer/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a6fb099295128f764ef492965fe57c81.js"
+  "/newsviewer/precache-manifest.beedece03241528d92f2f55fc9d43d97.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/newsviewer/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
